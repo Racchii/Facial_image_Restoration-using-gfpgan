2b2fe0345d80e1bf0cef10c740c7dfd05f721716
Racchii <rachitdeogirkar@gmail.com>
2026-05-09 10:34:32 +0530

    Add build.sh file

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "=== Upgrading pip ==="
+pip install --upgrade pip
+
+echo "=== Installing dependencies ==="
+pip install -r requirements.txt
+
+echo "=== Build completed successfully ==="
